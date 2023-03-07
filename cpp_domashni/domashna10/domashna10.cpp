24951978945888753608fb76ff7705a2bdf24fd1
AmraPacariz <72557449+AmraPacariz@users.noreply.github.com>
2023-03-07 04:35:01 +0100

    .

--- cpp_domashni/domashna10/domashna10.cpp
+++ cpp_domashni/domashna10/domashna10.cpp
@@ -4,7 +4,7 @@
 #include "Asistent.h"
 #include "Demonstrator.h"
 using namespace std;
-char opcija();
+char opcii();
 int main()
 {
     Asistent asistenti[10];
@@ -12,71 +12,74 @@ int main()
     Demonstrator demonstratori[10];
     int a = 0,p = 0,d = 0;
     char opcija=opcii();
-    while (opcija!='c')
+    while (opcija != 'c' )
     {
         switch (opcija)
         {
-        case 'a':
-            cout<<"a) Dodadi Vraboten "<<endl;
-            cout<<"Vnesi p-profesor a-asistent d-demonstrator"<<endl;
-            cin>>opcija;
-            if (opcija=='p')
-            {
-                ++p;
-                profesori[p-1].postavi();
-                 cout<<"Vnesen profesor"<<endl; 
-            }
-            else if (opcija=='a')
-            {
-                ++a;
-                asistenti[a-1].postavi();
-                 cout<<"Vnesen asistent"<<endl; 
-            }
-            else if (opcija=='d')
-            {
-                ++d;
-                demonstratori[d-1].postavi();
-                cout<<"Vnesen demonstrator"<<endl; 
-            }
-            else
-              cout<<"Greska vo vnesuvanje"<<endl;            
+            case 'a':
+                char opc;
+                cout<<"a) Dodadi Vraboten "<<endl;
+                cout<<"Vnesi p-profesor a-asistent d-demonstrator"<<endl;
+                cin>>opc;
+                    if (opc=='p')
+                    {
+                        ++p;
+                        profesori[p-1].postavi();
+                        cout<<"Vnesen profesor"<<endl; 
+                    }
+                    else if (opc=='a')
+                    {
+                        ++a;
+                        asistenti[a-1].postavi();
+                        cout<<"Vnesen asistent"<<endl; 
+                    }
+                    else if (opc=='d')
+                    {
+                        ++d;
+                        demonstratori[d-1].postavi();
+                        cout<<"Vnesen demonstrator"<<endl; 
+                    }
+                    else
+                    cout<<"Greska vo vnesuvanje"<<endl;            
+                    
+                break;
+            case 'b':
+                cout<<"PROFESORI:"<<endl;
+                if(p==0){cout<<"Nema Profesori"<<endl;}
+                for (int i = 0; i < p; i++)
+                {
+                    profesori[i].prikazi_podatociP();
+                }
+                cout<<"ASISTENTI:"<<endl;
+                if(a==0){cout<<"Nema Asistenti"<<endl;}
+                for (int i = 0; i < a; i++)
+                {
+                    asistenti[i].prikazi_podatociA();
+                }
+                cout<<"DEMONSTRATORI:"<<endl;
+                if(d==0){cout<<"Nema Demonstratori"<<endl;}
+                for (int i = 0; i < d; i++)
+                {
+                    demonstratori[i].prikazi_podatociD();
+                }
+                break;    
             
-            break;
-        case 'b':
-            cout<<"PROFESORI:"<<endl;
-            for (int i = 0; i < p; i++)
-            {
-                profesori[i].prikazi_podatociP();
-            }
-            cout<<"ASISTENTI:"<<endl;
-
-            for (int i = 0; i < a; i++)
-            {
-                asistenti[i].prikazi_podatociA();
-            }
-            cout<<"DEMONSTRATORI:"<<endl;
-
-            for (int i = 0; i < d; i++)
-            {
-                demonstratori[i].prikazi_podatociD();
-            }
-            break;    
-        
-        default:
-            break;
+            default:
+                break;
         }
+        opcija=opcii();
     }
 
 
 
-    opcija=opcii();
+    
     return 0;
 }
 
 char opcii()
 {
-    int choise;
-    cout<<"Izberi broj: "<<endl;
+    char choise;
+    cout<<"Izberi bukva: "<<endl;
     cout<<"a) Dodadi Vraboten "<<endl;
     cout<<"b) Prikazi vraboteni "<<endl;
     cout<<"c) Za kraj "<<endl;
